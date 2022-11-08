9a5c686b2e2ac06d4e83c1548d677c13dee5f0e1
unknown <maazq35@gmail.com>
2022-11-08 14:58:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-comm Project</h1>
+      <button> Normal Button </button>
+      <Button> Bootstrap button  </Button>
+    </div>
+  );
+}
+
+export default App;
